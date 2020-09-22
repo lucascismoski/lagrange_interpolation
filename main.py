2855f9e31ff8b0eb7b36af365985332d1bb0ebf9
Lucas Cismoski <lucascismoski@gmail.com>
2020-09-22 20:38:30 -0300

    Adicao do condicional para calculo direto

--- main.py
+++ main.py
@@ -6,43 +6,39 @@ import matplotlib.pyplot as plt
 #From the data in the following table, find the value
 #of P(50) by using Lagrange interpolation
 
-x = np.array([0, 20, 40, 60, 80, 100], float)
-y = np.array([26.0, 48.6, 61.6, 71.2, 74.8, 75.2], float)
+x = np.array([8.1, 8.3, 8.6, 8.7], float)
+y = np.array([16.94410, 17.56492, 18.50515, 18.82091], float)
 
+inp = input("Enter x: ")
+if inp == '':
+    xplt = np.linspace(x[0], x[-1])
+    yplt = np.array([], float)
 
-xplt = np.linspace(x[0],x[-1])
-yplt = np.array([],float)
+    for xp in xplt:
+        yp = 0
 
-for xp in xplt:
+        for xi, yi in zip(x, y):
+            yp += yi * np.prod((xp - x[x != xi])/(xi - x[x != xi]))
+        yplt = np.append(yplt, yp)
+
+    #plot
+    plt.plot(x, y, 'ro', xplt, yplt, 'b-')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+else:
+    xp = float(inp)
     yp = 0
 
-    for xi,yi in zip(x,y):
-        yp += yi * np.prod((xp - x[x != xi])/(xi - x[x != xi]))
-    yplt = np.append(yplt, yp)
+    for xi, yi in zip(x, y):
+        yp += yi * np.prod((xp - x[x != xi]) / (xi - x[x != xi]))
 
-#plot
-plt.plot(x,y,'ro',xplt,yplt,'b-')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+    print('For x = %.2f, y = %f' % (xp, yp))
 
 
 
 
-"""
-import numpy as np
 
-x = np.array([0, 20, 40, 60, 80, 100], float)
-y = np.array([26.0, 48.6, 61.6, 71.2, 74.8, 75.2], float)
-
-xp = float(input("Enter x: "))
-yp = 0 
-
-for xi,yi in zip(x,y):
-    yp += yi * np.prod((xp - x[x != xi])/(xi - x[x != xi]))
-
-print('For x = %.2f, y = %f' % (xp,yp))
-"""
 
 
 
